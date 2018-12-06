c3285da427dc25ee04bcdbe0dfd0c748ac1ed34a
Shane Walsh <walsh.shane@icloud.com>
2018-12-06 21:00:34 +0000

    Track winning combinations

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -10,17 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var winnerLabel: UILabel!
+    
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    
     // noughts = 1, crosses = 2
 
     var activePlayer = 1
     
+    var activeGame = true
+    
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0,]  // 0 - empty, 1 - noughts, 2 - crosses
+    
+    let winningCombinantions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     
     @IBAction func buttonPressed(_ sender: Any) {
         
         let activePosition = (sender as AnyObject).tag - 1
         
-        if gameState[activePosition] == 0 {
+        if gameState[activePosition] == 0 && activeGame {
             
             gameState[activePosition] = activePlayer
             
@@ -35,6 +44,19 @@ class ViewController: UIViewController {
                 (sender as AnyObject).setImage(UIImage(named: "cross.png"), for: [])
                 
                 activePlayer = 1
+            }
+            
+            for combination in winningCombinantions {
+                
+                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                    
+                    // We have a winner
+                    
+                    activeGame = false
+                    
+                    print(gameState[combination[0]])
+                    
+                }
             }
         }
        

b27e6c38c5ba5cd95b0632ecb3e7300a89d601da
Shane Walsh <walsh.shane@icloud.com>
2018-12-06 17:44:32 +0000

    Connect button actions

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        print("Button Pressed")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
